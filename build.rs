0e6d23357ffc55bf172d9438080c448a11dd1d98
HimbeerserverDE <himbeerserverde@gmail.com>
2023-12-04 18:55:45 +0100

    fix builds (remove libpcap libnl dependency)

--- build.rs
+++ build.rs
@@ -8,9 +8,13 @@ fn main() {
     // println!("cargo:rustc-link-search=./libpcap/rpi");
 
     match env::var("CARGO_CFG_TARGET_ARCH").unwrap_or(String::new()).as_str() {
-        "x86_64" => println!("cargo:rustc-link-search=./libpcap/x86_64"),
-        "aarch64" => println!("cargo:rustc-link-search=./libpcap/rpi"),
-        _ => println!("cargo:warning=Building for unsupported architecture, library search path may be incorrect"),
+        "x86_64" => {
+            println!("cargo:rustc-link-search=./libpcap/out/x86_64")
+        }
+        "aarch64" => {
+            println!("cargo:rustc-link-search=./libpcap/out/rpi")
+        }
+        _ => println!("cargo:warning=Building for unsupported architecture, library search paths may be incorrect"),
     }
 
     println!("cargo:rustc-link-lib=static=pcap");
