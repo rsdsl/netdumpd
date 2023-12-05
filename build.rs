46980958950c105650dd4d9a18f4bcec120b6e2e
HimbeerserverDE <himbeerserverde@gmail.com>
2023-12-05 16:31:50 +0100

    move libpcap to lib subdirectory

--- build.rs
+++ build.rs
@@ -9,10 +9,10 @@ fn main() {
 
     match env::var("CARGO_CFG_TARGET_ARCH").unwrap_or(String::new()).as_str() {
         "x86_64" => {
-            println!("cargo:rustc-link-search=./libpcap/out/x86_64")
+            println!("cargo:rustc-link-search=./lib/libpcap/out/x86_64")
         }
         "aarch64" => {
-            println!("cargo:rustc-link-search=./libpcap/out/rpi")
+            println!("cargo:rustc-link-search=./lib/libpcap/out/rpi")
         }
         _ => println!("cargo:warning=Building for unsupported architecture, library search paths may be incorrect"),
     }
